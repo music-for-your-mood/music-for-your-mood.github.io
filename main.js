29d4255e05b67063e54ce064363a901fa3c47a60
C4FFEIN3 <seonjm@changwon.ac.kr>
2021-11-22 23:42:34 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 function talkSizeChange() {
 	const talk = document.querySelector(".talkBox");
+	const talkbtn = document.querySelector(".talkBtn");
 	if (talk.style.height == "100%"){
-		talk.style.height = "50px";
+		talk.style.height = "10%";
+		talkbtn.style.height = "100%";
 	}
 	else {
 		talk.style.height = "100%";
+		talkbtn.style.height = "10%";
 	}
 }
