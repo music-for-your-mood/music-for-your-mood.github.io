dfebfe3cd2ba06605e8efa261a8dba8369e89439
c4ffein3 <seonjm@changwon.ac.kr>
2021-11-23 14:31:27 +0900

    Update main.js

--- main.js
+++ main.js
@@ -2,11 +2,35 @@ function talkSizeChange() {
 	const talk = document.querySelector(".talkBox");
 	const talkbtn = document.querySelector(".talkBtn");
 	if (talk.style.height == "100%"){
-		talk.style.height = "10%";
+		talk.style.height = "5%";
 		talkbtn.style.height = "100%";
 	}
 	else {
 		talk.style.height = "100%";
-		talkbtn.style.height = "10%";
+		talkbtn.style.height = "5%";
+	}
+}
+
+function ansMsg(usrMsg){
+	var ans = "aaa";
+	
+	document.querySelector(".msgViewer").innerHTML =
+				document.querySelector(".msgViewer").innerHTML
+				+ "<div class=\"msgService\">" + ans + "</div>";
+}
+
+function sendMsg(){
+	var msg = document.querySelector(".inputBody").value;
+	
+	document.querySelector(".msgViewer").innerHTML = 
+				document.querySelector(".msgViewer").innerHTML
+				+ "<div class=\"msgUser\">" + msg + "</div>";
+	document.querySelector(".inputBody").value = "";
+	ansMsg(msg);
+}
+
+function enterCheck(e){
+	if (e.key == "Enter" || e.keyCode == 13){
+		sendMsg();
 	}
 }
