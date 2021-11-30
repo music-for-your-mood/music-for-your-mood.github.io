3ee9db8efa77945248494fe8881e0544905c5608
c4ffein3 <seonjm@changwon.ac.kr>
2021-11-30 13:50:01 +0900

    Update ml.js

--- ml.js
+++ ml.js
@@ -39,8 +39,8 @@ function process(d){
 function run(raw_data){
     var data = process(raw_data);
     var pred = model.predict(data);
-	var emotion = pred[0].argMax();
-	var sit = pred[1].argMax();
+	var emotion = pred[0].reshape([-1]).argMax();
+	var sit = pred[1].reshape([-1]).argMax();
 	
 	console.log(emotion.print());
     return [emotion, sit];
